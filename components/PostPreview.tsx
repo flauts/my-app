7dc1453248b1fa6f02b4fa482189e55767d94466
flauta <hector.nieto@utec.edu.pe>
2024-07-25 14:22:14 -0500

    a

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -1,12 +1,32 @@
 import { styled } from 'nativewind';
-import { View, Text, TextInput } from 'react-native'
-
-
+import { View, Image } from 'react-native'
+import PagerView from 'react-native-pager-view';
 const PostPreview = () => {
+    const images = [
+        {
+            'title':'Secretive Plotter',
+            'url':'https://www.reddit.com/media?url=https%3A%2F%2Fi.redd.it%2Ftliimptmiai91.jpg'
+        },{
+            'title':'The Great Plotter',
+            'url':'https://i.pinimg.com/736x/a6/fc/1c/a6fc1ce13c67b789d94e6c84fd74e448.jpg'
+        },{
+            'title':'Chaos',
+            'url':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRD4SBpv0LNSlva5oaHcfT2AHpRrJvP8rRiEA&s'
+    
+        }
+    ]
     return (
         <View>
-        <Text>ComponentPost</Text>            
-        </View>
+            <PagerView initialPage={0}>
+            {images.map((image, index) => (
+                <View className='justify-center content-center' key={index} collapsable={false}>
+                    <Image
+                        source={{uri: image.url}}
+                        style={{width: 100, height: 100}}/>
+                    </View>
+            ))}
+            </PagerView>
+            </View>
     )
 }
 
