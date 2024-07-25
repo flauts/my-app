70df12a6b0db9e7e7c165d76c3edf858de06438e
flauta <hector.nieto@utec.edu.pe>
2024-07-25 17:59:48 -0500

    simulate regular behaviour home

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -2,30 +2,29 @@ import React from 'react';
 import { StyleSheet, View, Text,Image } from 'react-native';
 import PagerView from 'react-native-pager-view';
 
-const images = [
-    {
-        'title':'Secretive Plotter',
-        'url':'https://steamuserimages-a.akamaihd.net/ugc/1796352800264815295/F70FC39CBEEC303C3B138FD253DEE23E320FBC2C/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false'
-    },{
-        'title':'The Great Plotter',
-        'url':'https://i.pinimg.com/736x/a6/fc/1c/a6fc1ce13c67b789d94e6c84fd74e448.jpg'
-    },{
-        'title':'Chaos',
-        'url':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRD4SBpv0LNSlva5oaHcfT2AHpRrJvP8rRiEA&s'
+type Post ={
+    user:string;
+    title:string;
+    description:string;
+    images:{
+        url:string[];
+    };
+}
 
-    }
-]
 
-export default function PostPreview() {
+export default function PostPreview(post:Post) {
     return (
         <View style={styles.container}>
           <PagerView style={styles.container} initialPage={0}>
-            {images.map((image,index) =>(
+            {post.images.url.map((image_url,index) =>(
                 <View style={styles.page} key={index}>
-                    <Image source={{uri:image.url}} style={{width:'100%',height:'100%'}} />
+                    <Image source={{uri:image_url}} style={{width:'100%',height:'100%'}} />
                     </View>
             ))}
           </PagerView>
+          <Text>{post.title}</Text>
+          <Text>{post.description}</Text>
+
         </View>
       );
     }
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -4,13 +4,52 @@ import SearchBar from '../components/SearchBar'
 import Header from '../components/Header';
 import PostPreview from '../components/PostPreview';
 
+
+
 const HomeScreen = () => {
+    const feed = [
+        {
+            'user':'@user1',
+            'title':'Secretive Plotter',
+            'description':'Who rises up from the place of nothing',
+            'images':{
+                'url':['https://steamuserimages-a.akamaihd.net/ugc/1796352800264815295/F70FC39CBEEC303C3B138FD253DEE23E320FBC2C/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false',
+                'https://i.pinimg.com/736x/a6/fc/1c/a6fc1ce13c67b789d94e6c84fd74e448.jpg',
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRD4SBpv0LNSlva5oaHcfT2AHpRrJvP8rRiEA&s'
+                ]
+            }
+        },
+        {
+            'user':'@user2',
+            'title':'The Great Plotter',
+            'description':'Who rises up from the place of nothing',
+            'images':{
+                'url':['https://steamuserimages-a.akamaihd.net/ugc/1796352800264815295/F70FC39CBEEC303C3B138FD253DEE23E320FBC2C/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false',
+                'https://i.pinimg.com/736x/a6/fc/1c/a6fc1ce13c67b789d94e6c84fd74e448.jpg',
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRD4SBpv0LNSlva5oaHcfT2AHpRrJvP8rRiEA&s'
+                ]
+            }
+        },{
+            'user':'@user3',
+            'title':'Chaos',
+            'description':'Who rises up from the place of nothing',
+            'images':{
+                'url':['https://steamuserimages-a.akamaihd.net/ugc/1796352800264815295/F70FC39CBEEC303C3B138FD253DEE23E320FBC2C/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false',
+                'https://i.pinimg.com/736x/a6/fc/1c/a6fc1ce13c67b789d94e6c84fd74e448.jpg',
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRD4SBpv0LNSlva5oaHcfT2AHpRrJvP8rRiEA&s'
+                ]
+            }
+        }
+    ]
+
     const insets = useSafeAreaInsets();
     return (
         <View style={{flex:1,paddingTop:insets.top, paddingBottom:insets.bottom}}>
             <Header />
             <SearchBar />
-            <PostPreview />
+            {feed.map((post,index) => (
+                <PostPreview key={index} {...post} />
+            ))}
             </View>
     )
 }
